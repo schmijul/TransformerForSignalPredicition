09100bbd09cd811c36672ba961ac000ef8fabe6f
schmijul <julian.schmidt2@mailbox.tu-dresden.de>
2023-03-30 19:25:27 +0200

    aded comments

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import numpy as np
+import os
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.data import Dataset
 
 import pytorch_lightning as pl
 from pytorch_lightning import Trainer
+from pytorch_lightning.callbacks.early_stopping import EarlyStopping
+from pytorch_lightning.loggers import TensorBoardLogger
+
 
 from models.transformermodels import AutoregressiveTransformer
 from timeseries_dataset import TimeSeriesDataset
 
 
 def load_dataset(path="data/", noise="snr10", horizon=3):
+    """
+    This function loads the dataset from the specified path.
+    :param path: Path to the dataset
+    :param noise: SNR in dB
+    :param horizon: Prediction horizon ( ms)
+    
+    :return: train_data, val_data, test_data (dict)
+    """
     train_data = {
         "x": np.load(path + f"train_data/x_train_{noise}.npy"),
         "y": np.load(path + f"train_data/y_train_{noise}.npy")[:, horizon]
@@ -27,15 +39,20 @@ def load_dataset(path="data/", noise="snr10", horizon=3):
 
     return train_data, val_data, test_data
 
+
 if __name__ == "__main__":
     
-    BATCH_SIZE = 4*64
-    NUM_LAYERS = 2
-    N_HEAD = 4
+    BATCH_SIZE = 4*64 # Batch size
+    NUM_LAYERS = 2 # Number of transformer layers
+    N_HEAD = 2 # Number of heads in the multi-head attention layer
+    MAX_EPOCHS = 100 # Maximum number of epochs
+    PATIENCE = 10 # Early stopping patience if val loss does not improve after PATIENCE epochs stop Training
     
-    
-    train_data, val_data, test_data = load_dataset()
+    # Load the dataset
 
+    train_data, val_data, test_data = load_dataset() 
+    
+    
     train_dataset = TimeSeriesDataset(train_data["x"], train_data["y"])
     val_dataset = TimeSeriesDataset(val_data["x"], val_data["y"])
     test_dataset = TimeSeriesDataset(test_data["x"], test_data["y"])
@@ -44,9 +61,26 @@ if __name__ == "__main__":
     val_dataloader = DataLoader(val_dataset, batch_size=BATCH_SIZE, drop_last=True)
     test_dataloader = DataLoader(test_dataset, batch_size=BATCH_SIZE, drop_last=True)
 
-
+    # Initialize the model
     model = AutoregressiveTransformer(d_model=BATCH_SIZE, nhead=N_HEAD, num_layers=NUM_LAYERS, input_size=train_data["x"].shape[1])
-    trainer = Trainer(max_epochs=50)  # Use 'gpus=1' to run on a GPU, if available
+
+    # Initialize the trainer
+    logger = TensorBoardLogger(save_dir=os.getcwd(), version=1, name="lightning_logs")
+    
+    # Early stopping    
+    early_stop_callback = EarlyStopping(monitor='val_loss', patience=PATIENCE)
+
+
+
+    # Initialize the trainer
+    trainer = Trainer(max_epochs=MAX_EPOCHS,
+                      callbacks=[early_stop_callback],
+                      logger=logger)
+    # Training
     trainer.fit(model, train_dataloader, val_dataloader)
-    trainer.test(test_dataloaders=test_dataloader)
+    
+    # Testing
+    trainer.test(dataloaders=test_dataloader)
+    
+    # Predicting
     model.predict_test_set(test_dataloader)
--- models/transformermodels.py
+++ models/transformermodels.py
@@ -1,3 +1,5 @@
+import os
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -12,6 +14,10 @@ class AutoregressiveTransformer(LightningModule):
         self.linear = nn.Linear(input_size, d_model)
 
     def forward(self, x):
+        """
+        This function implements the forward pass of the model.
+        :param x: Input tensor of shape (batch_size, seq_len, input_size)
+        """
         x = self.linear(x)
         x = x.unsqueeze(1)
 
@@ -25,10 +31,20 @@ class AutoregressiveTransformer(LightningModule):
 
 
     def configure_optimizers(self):
+        """
+        This function configures the optimizer.
+        """
         optimizer = optim.Adam(self.parameters(), lr=1e-4)
         return optimizer
 
     def training_step(self, batch, batch_idx):
+        """
+        this function implements the training step.
+        :param batch: Batch of data
+        :param batch_idx: Batch index 
+
+        :return: Loss (torch.Tensor)
+        """
         x, y = batch
         y_hat = self(x)
         loss = nn.MSELoss()(y_hat, y)
@@ -36,12 +52,22 @@ class AutoregressiveTransformer(LightningModule):
         return loss
 
     def validation_step(self, batch, batch_idx):
+        """
+        This function implements the validation step.
+        :param batch: Batch of data
+        :param batch_idx: Batch index
+        """
         x, y = batch
         y_hat = self(x)
         loss = nn.MSELoss()(y_hat, y)
         self.log("val_loss", loss)
 
     def test_step(self, batch, batch_idx):
+        """
+        This function implements the test step.
+        :param batch: Batch of data
+        :param batch_idx: Batch index  
+        """
         x, y = batch
         y_hat = self(x)
         loss = nn.MSELoss()(y_hat, y)
@@ -49,6 +75,11 @@ class AutoregressiveTransformer(LightningModule):
         
         
     def predict_test_set(self, test_dataloader):
+        """
+        This function predicts the test set.
+        :param test_dataloader: Dataloader for the test set (torch.utils.data.DataLoader)
+        : return: Predictions   (numpy.ndarray)
+        """
         self.eval()  # Set the model to evaluation mode
         predictions = []
 
